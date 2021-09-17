9e2cced1a11484f92e9064aa6ea281f6bfad59ff
Kaitlyn Parkhurst <symkat@symkat.com>
2021-09-16 17:25:30 -0700

    DB changes.

--- Database/lib/OpenNewsWire/DB.pm
+++ Database/lib/OpenNewsWire/DB.pm
@@ -17,6 +17,10 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-09-06 15:34:11
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:R9/3GgzPrYil31aQ9mTKUw
 
+our $VERSION = 1;
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
+sub install_defaults {
+    my ( $self ) = @_;
+}
+
 1;
--- Web/lib/OpenNewsWire/Web/Model/DB.pm
+++ Web/lib/OpenNewsWire/Web/Model/DB.pm
@@ -6,7 +6,7 @@ extends 'Catalyst::Model::DBIC::Schema';
 
 __PACKAGE__->config(
     schema_class => 'OpenNewsWire::DB',
-    connect_info => [ 'HNW_DB' ],
+    connect_info => [ 'ONW_DB' ],
 );
 
 __PACKAGE__->meta->make_immutable;
